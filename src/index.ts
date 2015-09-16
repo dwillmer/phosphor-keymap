e3c9b562ea29516566b15e2b63dddcd923d76139
Dave Willmer <dave.willmer@gmail.com>
2015-09-16 20:47:17 +0100

    WIP

--- src/index.ts
+++ src/index.ts
@@ -7,17 +7,25 @@
 |----------------------------------------------------------------------------*/
 'use strict';
 
+import {
+  ISignal, Signal
+} from 'phosphor-signaling';
+
+
 /**
  * Maps a given key sequence to an identifier.
- * The identifier could be the id of a command, or any
- * other 
  */
-export IKeySequenceMap {
-
-    input: string;
-
-    id: string;
-
+export 
+interface IKeySequence {
+  /** 
+   * A string representing the key sequence, eg 'Ctrl-Shift-X'
+   */
+  input: string;
+  /**
+   * A string representing the id for an external component to use
+   * in processing the keyboard shortcut when activated.
+   */
+  id: string;
 }
 
 
@@ -35,14 +43,44 @@ interface IKeymapManager {
 }
 
 
+/**
+ * The options object for instantiating a keymap manager.
+ */
+export interface IKeymapManagerOptions {
+  /**
+   * The keyboard setting for this machine/user, 'mozilla' or 'ie'
+   */
+  keycodeSetting: string;
+  /**
+   * The external manager which will execute some code when a correct
+   * key sequence is pressed.
+   */
+  manager: any;
+}
 
 
+var MOZILLA_MODIFIERS: StringMap = { '59': ';', '61': '=', '224': 'meta', '173': 'minus' };
+var IE_MODIFIERS: StringMap = { '186': ';', '187': '=', '189': 'minus' };
+
+/**
+ * Convenience interface for string:string mappings
+ */ 
+export
+interface StringMap {
+  [s: string]: string;
+}
 
 /**
  * A convenience implementation of IKeymapManager
  *
  */
-export KeymapManager implements IKeymapManager {
+export 
+class KeymapManager implements IKeymapManager {
+  /**
+   * A signal emitted when the [[commandRequested]]
+   */
+  static commandRequestedSignal = new Signal<KeymapManager, string>();
+
   /**
    * Map of keycode to key character.
    *
@@ -52,19 +90,135 @@ export KeymapManager implements IKeymapManager {
    * We potentially have more than one Keyboard Manager in the app, so we 
    * don't want to store a copy of the keycodes list for each instance.
    */
-  static keycodes = {
-      65: 'a', 66: 'b', 67: 'c', 68: 'd', 69: 'e', 70: 'f', 71: 'g', 72: 'h', 73: 'i',
-      74: 'j', 75: 'k', 76: 'l', 77: 'm', 78: 'n', 79: 'o', 80: 'p', 81: 'q', 82: 'r',
-      83: 's', 84: 't', 85: 'u', 86: 'v', 87: 'w', 88: 'x', 89: 'y', 90: 'z',
-      49: '1', 50: '2', 51: '3', 52: '4', 53: '5', 54: '6', 55: '7', 56: '8', 57: '9',
-      48: '0',
-      219: '[', 221: ']', 192: '`', 188: ',', 190: '.', 191: '/', 220: '\\', 222: '\'',
-      220: '\\', 222: '\'',
-      96: 'numpad0', 97: 'numpad1', 98: 'numpad2', 99: 'numpad3', 100: 'numpad4',
-      101: 'numpad5', 102: 'numpad6', 103: 'numpad7', 104: 'numpad8', 105: 'numpad9',
-      106: 'multiply', 107: 'add', 109: 'subtract', 110: 'decimal', 111: 'divide', 
-      112: 'f1', 113: 'f2', 114: 'f3', 115: 'f4', 116: 'f5', 117: 'f6', 118: 'f7',
-      119: 'f8', 120: 'f9', 121: 'f10', 122: 'f11', 123: 'f12', 124: 'f13', 125: 'f14',
-      
+  static keycodes: StringMap = {
+    '65': 'a', '66': 'b', '67': 'c', '68': 'd', '69': 'e', '70': 'f', '71': 'g', 
+    '72': 'h', '73': 'i',
+    '74': 'j', '75': 'k', '76': 'l', '77': 'm', '78': 'n', '79': 'o', '80': 'p', 
+    '81': 'q', '82': 'r',
+    '83': 's', '84': 't', '85': 'u', '86': 'v', '87': 'w', '88': 'x', '89': 'y', 
+    '90': 'z',
+    '49': '1', '50': '2', '51': '3', '52': '4', '53': '5', '54': '6', '55': '7', 
+    '56': '8', '57': '9',
+    '48': '0',
+    '219': '[', '221': ']', '192': '`', '188': ',', '190': '.', '191': '/', '220': '\\', 
+    '222': '\'',
+    '96': 'numpad0', '97': 'numpad1', '98': 'numpad2', '99': 'numpad3', '100': 'numpad4',
+    '101': 'numpad5', '102': 'numpad6', '103': 'numpad7', '104': 'numpad8', '105': 'numpad9',
+    '106': 'multiply', '107': 'add', '109': 'subtract', '110': 'decimal', '111': 'divide',
+    '112': 'f1', '113': 'f2', '114': 'f3', '115': 'f4', '116': 'f5', '117': 'f6', '118': 'f7',
+    '119': 'f8', '120': 'f9', '121': 'f10', '122': 'f11', '123': 'f12', '124': 'f13', '125': 'f14',
+    '126': 'f15',
+    '8': 'backspace', '9': 'tab', '13': 'enter', '16': 'shift', '17': 'ctrl', '18': 'alt',
+    '91': 'meta', '20': 'capslock', '27': 'esc', '32': 'space', '33': 'pageup', '34': 'pagedown',
+    '35': 'end', '36': 'home', '37': 'left', '38': 'up', '39': 'right', '40': 'down', '45': 'insert',
+    '46': 'delete', '144': 'numlock'
+  };
+
+  /**
+   * Modifier keys for sequences
+   */
+  static modifierKeys: string[] = ['ctrl', 'alt', 'shift'];
+
+  /**
+   * Construct a new Keymap Manager.
+   */
+  constructor(options: IKeymapManagerOptions) {
+
+    if(options.keycodeSetting === 'mozilla') {
+      this._keycodeModifications = MOZILLA_MODIFIERS;
+    } else if(options.keycodeSetting === 'ie') {
+      this._keycodeModifications === IE_MODIFIERS;
+    }
+
+    this._commandManager = options.manager;
+    this._bindEvents();
   }
+
+  get commandRequested(): ISignal<KeymapManager, string> {
+    return KeymapManager.commandRequestedSignal.bind(this);
+  }
+
+  registerSequence(sequence: IKeySequence): boolean {
+    var input = this._mapKeyFromUserString(sequence.input);
+
+    if (this._keySequenceMap[input] !== undefined) {
+      console.warn("Keyboard shortcut already set: " + sequence.input);
+      return false;
+    }
+
+    console.log('REGISTERING: ' + input);
+    this._keySequenceMap[input] = sequence.id;
+    return true;
+  }
+
+  hasShortcut(value: string): boolean {
+    var input = this._mapKeyFromUserString(value);
+    console.log('has shortcut map key: ' + input);
+    return input in this._keySequenceMap;
+  }
+
+  private _mapKeyFromUserString(input: string): string {
+    var lowerInput = input.toLowerCase();
+    var seqItems = lowerInput.split('-');
+    var keyString = seqItems[seqItems.length - 1];
+    var modString = this._getModifierStringForSequence(lowerInput);
+    return modString + keyString;
+  }
+
+  private _getKeyChars(code: number): string {
+    var strCode = code.toString();
+    if(strCode in this._keycodeModifications) {
+      return this._keycodeModifications[strCode];
+    } else if(strCode in KeymapManager.keycodes) { 
+      return KeymapManager.keycodes[strCode];
+    }
+    console.error('Unrecognised keycode: ' + code.toString());
+  }
+
+  private _getModifierStringForSequence(value: string): string {
+    var lowerValue = value.toLowerCase();
+    var result = '';
+    for (var i = 0; i < KeymapManager.modifierKeys.length; i++) {
+      result += lowerValue.indexOf(KeymapManager.modifierKeys[i]) > -1 ? '1' : '0';
+    }
+    return result; 
+  }
+
+  private _getModifierStringForEvent(event: KeyboardEvent): string {
+    var isCtrl = <boolean>(event.ctrlKey);
+    var isAlt = <boolean>(event.altKey);
+    var isShift = <boolean>(event.shiftKey);
+
+    var result = '';
+    result += isCtrl ? '1' : '0';
+    result += isAlt ? '1' : '0';
+    result += isShift ? '1' : '0';
+    return result;
+  }
+
+  private _bindEvents(): void {
+    var that = this;
+    document.addEventListener("keydown", function(event: KeyboardEvent) {
+      
+      var key = <number>(event.keyCode);
+      var mods = that._getModifierStringForEvent(event);
+      var keyStr = that._getKeyChars(key);
+      var joinedKey = mods + keyStr;
+
+      console.log('KEYDOWN REGISTERED: ' + joinedKey);
+
+      if(joinedKey in that._keySequenceMap) {
+        var command = that._keySequenceMap[joinedKey];
+        console.log('ID found: ' + command);
+        that.commandRequested.emit(command);
+      }
+
+
+    });
+
+  }
+
+  private _keycodeModifications: StringMap = {};
+  private _keySequenceMap: StringMap = {};
+  private _commandManager: any;
 }
--- test/src/index.ts
+++ test/src/index.ts
@@ -9,12 +9,160 @@
 
 import expect = require('expect.js');
 
+import {
+KeymapManager, IKeySequence
+} from '../../lib/index';
+
+
+/**
+ * Helper function to generate keyboard events for unit-tests.
+ */
+var genKeyboardEvent = function(options: any): KeyboardEvent {
+  var keyEvent = <KeyboardEvent>document.createEvent("KeyboardEvent");
+  var initMethod = typeof keyEvent.initKeyboardEvent !== 'undefined' ? "initKeyboardEvent" : "initKeyEvent";
+  keyEvent[initMethod](
+    "keydown",
+    <boolean>(options.bubbles) || true,
+    <boolean>(options.cancelable) || true,
+    <Window>window,
+    <boolean>(options.ctrlKey) || false,
+    <boolean>(options.altKey) || false,
+    <boolean>(options.shiftKey) || false,
+    <boolean>(options.metaKey) || false,
+    <number>(options.keyCode),
+    <number>(options.charCodeArgs) || 0
+  );
+  return <KeyboardEvent>keyEvent;
+}
+
 
 describe('phosphor-keymap', () => {
   
-  describe('testing stub', () => { 
+  describe('KeymapManager', () => {
 
-    it('should always pass', () => {
+    describe('#keycodes mozilla', () => {
+
+      it('should register 59 for semi-colon', () => {
+
+        var options = {
+            keycodeSetting: 'mozilla',
+            manager: () => { console.error('Should not be called'); }
+        };
+        var km = new KeymapManager(options);
+
+        var testId = 'test:id';
+        var testInput = "Ctrl-;";
+        var sequence = {
+            input: testInput,
+            id: testId
+        };
+
+        var preRegistration = km.hasShortcut(testInput);
+        expect(preRegistration).to.be(false);
+
+        km.registerSequence(sequence);
+
+        var postRegistration = km.hasShortcut(testInput);
+        expect(postRegistration).to.be(true);
+
+        var id = '';
+        var handler = (sender: any, value: string) => {
+            id = value;
+        };
+        km.commandRequested.connect(handler, this);
+
+        expect(id).to.be('');
+
+        var keyEvent = genKeyboardEvent({ keyCode: 59, ctrlKey: true });
+        document.dispatchEvent(keyEvent);
+
+        expect(id).to.be(testId);
+
+      });
+
+    });
+
+    describe('#modifiers mozilla', () => {
+
+      it('should register permutations of ctrl-alt-shift', () => {
+
+        var options = {
+            keycodeSetting: 'mozilla',
+            manager: () => { console.error('Should not be called'); }
+        };
+        var km = new KeymapManager(options);
+
+        var testId = "test:id";
+        var testInput = "Ctrl-Shift-x";
+        var sequence = {
+            input: testInput,
+            id: testId
+        };
+
+        var preRegistration = km.hasShortcut(testInput);
+        expect(preRegistration).to.be(false);
+
+        var regSeq = km.registerSequence(sequence);
+        expect(regSeq).to.be(true);
+
+        var postRegistration = km.hasShortcut(testInput);
+        expect(postRegistration).to.be(true);
+
+        var alternate = km.hasShortcut('Shift-Ctrl-X');
+        expect(alternate).to.be(true);
+
+        var cas2 = "Ctrl-Alt-Shift-2";
+        var sequence = {
+            input: cas2,
+            id: testId
+        };
+
+        var preRegistration = km.hasShortcut(cas2);
+        expect(preRegistration).to.be(false);
+
+        km.registerSequence(sequence);
+
+        var postRegistration = km.hasShortcut(testInput);
+        expect(postRegistration).to.be(true);
+
+        var alternate1 = km.hasShortcut('Alt-ctrl-shift-2');
+        expect(alternate1).to.be(true);
+
+        var alternate2 = km.hasShortcut('Ctrl-Alt-Shift-23');
+        expect(alternate2).to.be(false);
+
+      });
+
+    });
+
+    describe('#registerSequence mozilla', () => {
+
+      it('should not overwrite a key sequence', () => {
+
+        var options = {
+          keycodeSetting: 'mozilla',
+          manager: () => { console.error('Should not be called'); }
+        };
+        var km = new KeymapManager(options);
+
+        var testId = "test:id";
+        var testInput = "Ctrl-Alt-L";
+        var sequence = {
+          input: testInput,
+          id: testId
+        };
+
+        var preRegistration = km.hasShortcut(testInput);
+        expect(preRegistration).to.be(false);
+
+        var regSeq = km.registerSequence(sequence);
+        expect(regSeq).to.be(true);
+
+        var postRegistration = km.hasShortcut(testInput);
+        expect(postRegistration).to.be(true);
+
+
+      });
 
     });
 
